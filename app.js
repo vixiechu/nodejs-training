cc1a08b4556edbf9f29a9c3d48fafb2d505c2b3c
Vix Morrigan <vixmorrigan@gmail.com>
2019-06-20 14:47:14 +0100

    readline module practice/interactive terminal

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ get name(){
 let vix = new Person('Vix');
 let morrigan = new Person('Morrigan');
 
-morrigan.on('name', ()=> {
+morrigan.on('name', () => {
   console.log('Hello, I am ' + morrigan.name);
 })
 
-vix.on('name', ()=> {
+vix.on('name', () => {
   console.log('Hello, I am ' + vix.name);
 })
 
@@ -37,3 +37,40 @@ vix.on('name', ()=> {
 
 vix.emit('name'); // executed first
 morrigan.emit('name'); // executed second
+
+//readline Module
+
+const readline = require('readline');
+const rl = readline.createInterface({  
+  input : process.stdin,
+  output : process.stdout });
+
+let num1 = Math.floor((Math.random() * 10) + 1);
+let num2 = Math.floor((Math.random() * 10) + 1);
+let answer = num1 + num2;
+
+rl.question(`What is ${ num1 } + ${ num2 }? \n`,
+(userInput) => {
+  if(userInput.trim() == answer) {
+    rl.setPrompt(`Wow - You got it! \n`);
+    rl.prompt();
+    rl.close();
+
+  }
+  else {
+    rl.setPrompt(`Whoops! That is incorrect - please try again \n`);
+    rl.prompt();
+    rl.on('line', (userInput) => {
+      if(userInput.trim() == answer)
+      rl.close();
+      else {
+        rl.setPrompt(`Whoops! Your answer of ${ userInput} is incorrect - please try again \n`);
+        rl.prompt();
+      }
+    })
+  }
+})
+
+rl.on('close', () => {
+  console.log('That is correct!');
+});
