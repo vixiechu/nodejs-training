79cc0051b3dfdf95b67b905ac374542dd2752986
Vix Morrigan <vixmorrigan@gmail.com>
2019-06-20 09:50:02 +0100

    initial commit

---
+++ tutorial.js
@@ -0,0 +1,16 @@
+const sum = (num1, num2) => num1 + num2;
+
+const PI = 3.14;
+
+class MathObject {
+  constructor() {
+    console.log('object created');
+  }
+}
+// individually make available/export - long winded & repetitive
+// module.exports.sum = sum; //tells node to make available elsewhere
+// module.exports.PI = PI;
+// module.exports.MathObject = MathObject;
+
+// can be done this way - cleaner code
+module.exports = {sum : sum, PI : PI, MathObject : MathObject};
